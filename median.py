4062a9377f2a52b596d14d4ebb8f7dbbb9cbce2b
Zahra Amaan <k21011879@kcl.ac.uk>
2022-10-03 11:23:40 +0100

    Update median.py

--- median.py
+++ median.py
@@ -5,7 +5,7 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        
+        numbers.sort()
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
